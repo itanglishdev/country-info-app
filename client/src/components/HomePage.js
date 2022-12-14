cdd2e26662f3123042d3470d6e3561c58836472f
itanglishdev <chrishethertonweb@gmail.com>
2022-12-14 19:13:28 +0100

    css fix

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -39,12 +39,12 @@ const HomePage = () => {
   return (
     <div className="main-page">
       <div className="main-page__top">
-      <h1>Welcome to Bomb the Country Info app</h1>
+      <h1>Welcome to the Country Info app</h1>
       <p>This interactive info about Countries app is designed to give you information on Countries. You just simply add the country's name and you will get general information about it, pin pointed on the World's map.</p>
       <p>Type in the box below and give it a try!</p>
       </div>
       <div className="input-container">
-      {!countryExists && <h1 className="doesnt-exist">Not enough Info</h1>}
+      {/* {!countryExists && <h1 className="doesnt-exist">Not enough Info</h1>} */}
       <input
         className="input-field"
         onChange={changeHandlers}
