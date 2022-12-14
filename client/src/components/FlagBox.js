5940493c512ff11edda62c94adce3458c5e36158
itanglishdev <chrishethertonweb@gmail.com>
2022-12-14 15:54:03 +0100

    fixed flag and branch

--- client/src/components/FlagBox.js
+++ client/src/components/FlagBox.js
@@ -11,8 +11,7 @@ function FlagBox() {
   return (  
     <div className="flag-box">
       <img src={selectedCountry.flag} alt=""  className="flag-box__flag"/>
-      <p>{`The phone access code to the country is +${selectedCountry.callingCodes[0]}.`}</p>
-      <p>{` The top level Website domain here is "${selectedCountry.topLevelDomain[0]}".`}</p>
+      
     </div>
   );
 }
--- client/src/components/InfoBox.js
+++ client/src/components/InfoBox.js
@@ -11,14 +11,13 @@ function InfoBox() {
   console.log('countries',selectedCountry)
   
   // const countryDetails = countryInfo.map((el, id) => {
-
+ const population = (selectedCountry.population/ 1000000).toFixed(2)
   // })
   return ( 
-    <div className="info-box"  >
+    <div className="info-box">
       <h3  className="info-box__main"><strong>{selectedCountry.name}</strong></h3>
-      <p>{`The native name is ${selectedCountry.nativeName} and the local language is ${selectedCountry.languages[0].name}. The capital is ${selectedCountry.capital}. It is located in ${selectedCountry.region} in the sub-region of ${selectedCountry.subregion}. `} </p>
-      <p>{`The population is ${selectedCountry.population}, the local currency is ${selectedCountry.currencies[0].name}/${selectedCountry.currencies[0].code} (${selectedCountry.currencies[0].symbol}).`}</p>
-      <p>{`Time zone is ${selectedCountry.timezones[0]}`}</p>
+      <p>{`The native name is ${selectedCountry.nativeName} and the local language is ${selectedCountry.languages[0].name}. The capital is ${selectedCountry.capital}. It is located in ${selectedCountry.region} in the sub-region of ${selectedCountry.subregion}. The population is of circa ${population} millions. The local currency is ${selectedCountry.currencies[0].name}/${selectedCountry.currencies[0].code} (${selectedCountry.currencies[0].symbol}).`}</p>
+      <p >{` Time zone is ${selectedCountry.timezones[0]}. The phone access code to the country is +${selectedCountry.callingCodes[0]}. The top level Website domain here is "${selectedCountry.topLevelDomain[0]}".`}</p>
     </div>
    );
 }
